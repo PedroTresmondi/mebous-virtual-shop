0a2df2e49d1ca4ef3cbfe021c7cbb425fe4ddd2e
PedroTresmondi <pedro_tresmondi@hotmail.com>
2023-06-14 15:42:27 -0300

    Adicionando lista de produtos e cardProduto

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import AppLayout from 'src/components/Layout'
+import 'src/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+    return (
+        <AppLayout>
+            <Component {...pageProps} />
+        </AppLayout>
+    )
+}
---
+++ src/utils/stripe.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+import Stripe from "stripe";
+
+export const stripe = Stripe(`${process.env.STRIPE_SECRET_KEY}`)
